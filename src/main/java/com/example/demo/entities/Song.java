f25f068597890851e8cd8559e4013369cca64089
INDIA <INDIA@DESKTOP-56K1O10>
2024-01-18 14:31:36 +0530

    1st version of tune hub

---
+++ src/main/java/com/example/demo/entities/Song.java
@@ -0,0 +1,90 @@
+package com.example.demo.entities;
+
+import java.util.List;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Song {
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	int id;
+	String name;
+	String artist;
+	String genre;
+	String gender;
+	String link;
+	@ManyToMany
+	List<Playlist>playlists;
+	public Song() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Song(int id, String name, String artist, String genre, String gender, String link,
+			List<Playlist> playlists) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.artist = artist;
+		this.genre = genre;
+		this.gender = gender;
+		this.link = link;
+		this.playlists = playlists;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getArtist() {
+		return artist;
+	}
+	public void setArtist(String artist) {
+		this.artist = artist;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getLink() {
+		return link;
+	}
+	public void setLink(String link) {
+		this.link = link;
+	}
+	public List<Playlist> getPlaylists() {
+		return playlists;
+	}
+	public void setPlaylists(List<Playlist> playlists) {
+		this.playlists = playlists;
+	}
+	@Override
+	public String toString() {
+		return "Song [id=" + id + ", name=" + name + ", artist=" + artist + ", genre=" + genre + ", gender=" + gender
+				+ ", link=" + link + ", playlists=" + playlists + "]";
+	}
+	
+
+
+
+}
